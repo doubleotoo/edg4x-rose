13434b80bdf3f511844a7d8bb1b0bd7e0ce4fe0a
Justin Too <doubleotoo@gmail.com>
2013-05-17 14:01:47 -0700

    (Java) Add SgJavaPackageStatement IR Node

--- src/frontend/SageIII/sageInterface/sageInterface.C
+++ src/frontend/SageIII/sageInterface/sageInterface.C
@@ -1100,6 +1100,17 @@ SageInterface::get_name ( const SgDeclarationStatement* declaration )
                break;
              }
 
+        // DQ (4/16/2011): Added Java import statment support.
+           case V_SgJavaPackageStatement:
+             {
+               name = "_java_package_stmt_";
+               const SgJavaPackageStatement* statement = isSgJavaPackageStatement(declaration);
+               ROSE_ASSERT(statement != NULL);
+               ROSE_ASSERT(statement->get_parent() != NULL);
+               name += StringUtility::numberToString(const_cast<SgJavaPackageStatement*>(statement));
+               break;
+             }
+
        // Note that the case for SgVariableDeclaration is not implemented
           default:
             // name = "default name (default case reached: not handled)";
